2c324f934eb6231f9ce4ad102d4ec46cd0f87192
brotzeit <brotzeitmacher@gmail.com>
2018-05-23 22:53:56 +0200

    rustic-playpen: one function for region, src-block and buffer contents

--- rustic-util.el
+++ rustic-util.el
@@ -196,37 +196,40 @@
 ;;;;;;;;;;;;;;;;
 ;; Interactive
 
-(defun rustic-playpen-region (begin end)
-  "Create a sharable URL for the contents of the current region
-   on the Rust playpen."
+;;;###autoload
+(defun rustic-playpen (begin end)
+  "Create a shareable URL for the contents of the current region, 
+src-block or buffer on the Rust playpen."
   (interactive "r")
-  (let* ((data (buffer-substring begin end))
-         (escaped-data (url-hexify-string data))
-         (escaped-playpen-url (url-hexify-string (format rustic-playpen-url-format escaped-data))))
-    (if (> (length escaped-playpen-url) 5000)
-        (error "encoded playpen data exceeds 5000 character limit (length %s)"
-               (length escaped-playpen-url))
-      (let ((shortener-url (format rustic-shortener-url-format escaped-playpen-url))
-            (url-request-method "POST"))
-        (url-retrieve shortener-url
-                      (lambda (state)
-                        ;; filter out the headers etc. included at the
-                        ;; start of the buffer: the relevant text
-                        ;; (shortened url or error message) is exactly
-                        ;; the last line.
-                        (goto-char (point-max))
-                        (let ((last-line (thing-at-point 'line t))
-                              (err (plist-get state :error)))
-                          (kill-buffer)
-                          (if err
-                              (error "failed to shorten playpen url: %s" last-line)
-                            (message "%s" last-line)))))))))
-
-(defun rustic-playpen-buffer ()
-  "Create a sharable URL for the contents of the current buffer
-   on the Rust playpen."
-  (interactive)
-  (rustic-playpen-region (point-min) (point-max)))
+  (let (data)
+    (cond 
+     ((region-active-p)
+      (setq data (buffer-substring begin end)))
+     ((org-in-src-block-p)
+      (setq data (org-element-property :value (org-element-at-point))))
+     (t
+      (setq data (buffer-substring (point-min) (point-max)))))
+    (let* ((escaped-data (url-hexify-string data))
+           (escaped-playpen-url (url-hexify-string (format rust-playpen-url-format escaped-data))))
+      (if (> (length escaped-playpen-url) 5000)
+          (error "encoded playpen data exceeds 5000 character limit (length %s)"
+                 (length escaped-playpen-url))
+        (let ((shortener-url (format rust-shortener-url-format escaped-playpen-url))
+              (url-request-method "POST"))
+          (url-retrieve shortener-url
+                        (lambda (state)
+                          ;; filter out the headers etc. included at the
+                          ;; start of the buffer: the relevant text
+                          ;; (shortened url or error message) is exactly
+                          ;; the last line.
+                          (goto-char (point-max))
+                          (let ((last-line (thing-at-point 'line t))
+                                (err (plist-get state :error)))
+                            (kill-buffer)
+                            (if err
+                                (error "failed to shorten playpen url: %s" last-line)
+                              (let ((URL (read-from-minibuffer "Playpen URL: " last-line)))
+                                (browse-url URL)))))))))))
 
 ;;;###autoload
 (defun rustic-cargo-build ()
