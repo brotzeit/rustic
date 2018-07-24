e92328b879d43a6983c996d093a9d74629c13825
brotzeit <brotzeitmacher@gmail.com>
2018-07-24 11:58:28 +0200

    change default rls package to lsp-mode
    I think most people still use lsp-mode + flycheck

--- rustic.el
+++ rustic.el
@@ -49,7 +49,7 @@
 ;;;;;;;;;;;;;;;;;;
 ;; Customization
 
-(defcustom rustic-rls-pkg 'eglot
+(defcustom rustic-rls-pkg 'lsp-mode
   "Emacs package for interaction with rls."
   :type '(choice (symbol :tag 'eglot "eglot")
                  (symbol :tag 'lsp-mode "lsp-mode"))
