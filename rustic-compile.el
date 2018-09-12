34b67c6aa285dd166f356b53f398f07af64553a5
brotzeit <brotzeitmacher@gmail.com>
2018-09-12 14:44:48 +0200

    compilation--ensure-parse: always pass end of buffer as limit

--- rustic-compile.el
+++ rustic-compile.el
@@ -331,6 +331,15 @@ buffers are formatted after saving if `rustic-format-on-save' is t."
 
 (advice-add 'save-some-buffers :around #'rustic-save-some-buffers-advice)
 
+;; TODO: there has to be a better way to do this
+;; fix multiline error parsing
+(defun rustic-compilation--ensure-parse-advice (orig-fun &rest args)
+  (let ((limit (point-max)))
+    (apply orig-fun (list limit))))
+
+(advice-add 'compilation--ensure-parse :around #'rustic-compilation--ensure-parse-advice)
+
+
 ;;;;;;;;;;
 ;; Rustc
 
