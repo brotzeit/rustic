67881ba35b5694d169bc4be4f83bac14c6532e48
brotzeit <brotzeitmacher@gmail.com>
2018-10-17 12:34:54 +0200

    test error count

--- test/rustic-compile-test.el
+++ test/rustic-compile-test.el
@@ -61,3 +61,63 @@
     (while (eq (process-status proc) 'run)
       (sit-for 0.1))
     (should (= 0 (process-exit-status proc)))))
+
+(defun rustic-test-count-error-helper (string)
+  (let* ((buffer (get-buffer-create "b"))
+         (dir (rustic-babel-generate-project t))
+         (src (concat dir "/src"))
+         (file (expand-file-name "main.rs" src))
+         (buffer-save-without-query t)
+         (rustic-format-on-save nil))
+    (with-current-buffer buffer
+      (write-file file)
+      (insert string)
+      (save-buffer))
+    dir))
+
+(ert-deftest rustic-test-backtrace ()
+  (let* ((string "fn main() {
+                       let v = vec![1, 2, 3];
+                       v[99];
+                    }")
+         (default-directory (rustic-test-count-error-helper string)))
+    (let ((rustic-compile-backtrace "0")
+          (proc (rustic-compilation-start (split-string "cargo run"))))
+      (while (eq (process-status proc) 'run)
+        (sit-for 0.1))
+      (with-current-buffer (get-buffer rustic-compilation-buffer-name)
+        (should (= compilation-num-errors-found 1))))
+    (let ((rustic-compile-backtrace "1")
+          (proc (rustic-compilation-start (split-string "cargo run"))))
+      (while (eq (process-status proc) 'run)
+        (sit-for 0.1))
+      (with-current-buffer (get-buffer rustic-compilation-buffer-name)
+        (should (= compilation-num-errors-found 1))))
+    (let ((rustic-compile-backtrace "full")
+          (proc (rustic-compilation-start (split-string "cargo run"))))
+      (while (eq (process-status proc) 'run)
+        (sit-for 0.1))
+      (with-current-buffer (get-buffer rustic-compilation-buffer-name)
+        (should (= compilation-num-errors-found 1))))))
+
+(ert-deftest rustic-test-count-errors ()
+  ;; test error without error code
+  (let* ((string "fn main() {")
+         (default-directory (rustic-test-count-error-helper string)))
+    (let ((proc (rustic-compilation-start (split-string "cargo build"))))
+      (while (eq (process-status proc) 'run)
+        (sit-for 0.1))
+      (with-current-buffer (get-buffer rustic-compilation-buffer-name)
+        (should (= compilation-num-errors-found 1))))))
+
+(ert-deftest rustic-test-count-warnings ()
+  (let* ((string "fn main() {
+                       let v1 = vec![1, 2, 3];
+                       let v2 = vec![1, 2, 3];
+                    }")
+         (default-directory (rustic-test-count-error-helper string))
+         (proc (rustic-compilation-start (split-string "cargo build"))))
+    (while (eq (process-status proc) 'run)
+      (sit-for 0.1))
+    (with-current-buffer (get-buffer rustic-compilation-buffer-name)
+      (should (= compilation-num-warnings-found 2)))))
