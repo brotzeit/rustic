149594bbc483de05b33c8b564a5487d1d6a40f5b
Jonas Bernoulli <jonas@bernoul.li>
2020-02-24 13:14:13 +0100

    Add rustic-common.el for utilities shared by other libraries

---
+++ rustic-common.el
@@ -0,0 +1,12 @@
+;;; rustic-common.el --- Common things -*-lexical-binding: t-*-
+;;; Commentary:
+
+;; This library defines things that are shared between different other
+;; libraries, which may not be related to one another.  This is a bit
+;; of a kludge and it may become unnecessary over time.
+
+;;; Code:
+
+;;; _
+(provide 'rustic-common)
+;;; rustic-common.el ends here
