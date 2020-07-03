72431bb69002dc19f05fea5c0314be1890f9f553
brotzeit <brotzeitmacher@gmail.com>
2020-07-03 22:18:09 +0200

    allow sending input to process with rustic-compile-send-input

--- rustic-compile.el
+++ rustic-compile.el
@@ -408,6 +408,16 @@ current line in an error section."
 
 (advice-add 'compile-goto-error :around #'rustic-compile-goto-error-hook)
 
+(defun rustic-compile-send-input ()
+  "Read string from minibuffer and send it to the rust process of the current
+buffer."
+  (interactive)
+  (let ((input (read-from-minibuffer "Send input to rust process: "))
+        (proc (get-buffer-process (current-buffer)))
+        (inhibit-read-only t))
+    (process-send-string (concat input "\n"))))
+
+
 ;;; Rustc
 
 (defface rustic-errno-face
