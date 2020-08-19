4185fd50a7284231fd4a8600a2892adedf4cd562
Sam Hedin <sam.hedin@gmail.com>
2020-08-19 22:42:36 +0200

    Rustdoc resources

---
+++ rustdoc/convert.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+LUA_FILTER="$HOME/.local/bin/rustdoc-filter.lua"
+
+function num_cpus {
+    rg -c '^$' /proc/cpuinfo
+}
+
+function get_toolchain {
+    rustup show | sed -nr 's/(.*) \(default\)/\1/p' | head -n 1
+}
+
+if [ "$1" = "" ] || [ "$1" = "--help"  ]; then
+    MY_NAME="$(basename "$0")"
+    echo "Usage:"
+    echo "  $MY_NAME <library>"
+    echo "  $MY_NAME <docs src> <docs org dst>"
+    exit 0
+fi
+
+DOC_PATH="$1"
+DEST_DIR="$2"
+
+if [ "$DEST_DIR" = "" ]; then
+    LIBRARY="$1"
+    TARGET="$(get_toolchain)"
+
+    ## Users can change the location of the rustup directory
+    RUSTUP_HOME="${RUSTUP_HOME:-$HOME/.rustup}"
+    ## Set
+    DOC_PATH="$RUSTUP_HOME/toolchains/$TARGET/share/doc/rust/html/$LIBRARY"
+    DEST_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/emacs/rustdoc/$LIBRARY"
+
+    echo "Generating org files in: $DEST_DIR"
+fi
+
+mkdir -p "$DEST_DIR" || exit 1
+cd "$DOC_PATH" || exit 1
+
+## Copy directory structure
+fd . -td -x mkdir -p "$DEST_DIR/{}"
+
+## Find redirect files (removes $DOC_PATH prefix)
+ignore_file="$(mktemp)"
+rg -l "<p>Redirecting to <a href=\"[^\"]*\"" "$DOC_PATH" | \
+    awk -v PRE="$DOC_PATH" '
+BEGIN { m = length(PRE)
+        if (!match(PRE, /\/$/))
+           m += 1 }
+{ print substr($0, m+1) }
+' \
+    > "$ignore_file"
+
+## Convert files
+fd . \
+    -ehtml \
+    --ignore-file "$ignore_file" \
+    -j"$(num_cpus)" \
+    -x pandoc '{}' \
+    --lua-filter "$LUA_FILTER" \
+    -o "$DEST_DIR/{.}.org"
