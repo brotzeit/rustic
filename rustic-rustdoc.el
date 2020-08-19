e7a4e3d8158c8bf2d2c02bd8e5195c252e477b73
Sam Hedin <sam.hedin@gmail.com>
2020-08-19 22:48:13 +0200

    Update source repo location

--- rustic-rustdoc.el
+++ rustic-rustdoc.el
@@ -63,10 +63,7 @@
                                      ".local/bin/rustdoc-convert.sh")
   "Save location for the rustdoc conversion script.")
 
-(defvar rustdoc-source-user "samhedin")
-
-(defvar rustdoc-source-repo (format "https://raw.githubusercontent.com/%s/rustdoc-to-org/master/"
-                                    rustdoc-source-user))
+(defvar rustdoc-source-repo "https://raw.githubusercontent.com/samhedin/rustic/rustic-rustdoc/rustdoc/")
 
 (defvar rustdoc-current-project nil "Location to search for documentation.
 All projects and std by default, otherwise last open project and std.")
