63177a8d3acc3f8805194f5a907ad5e184909a94
Sam Hedin <sam.hedin@gmail.com>
2020-11-01 09:11:39 +0100

    Restore old behavior when cargo workspace is nil

--- rustic-common.el
+++ rustic-common.el
@@ -54,7 +54,6 @@ non-nil."
   (or rustic-format-on-save (eq rustic-format-trigger 'on-save)))
 
 ;;; Essentials
-
 (defun rustic-buffer-workspace (&optional nodefault)
   "Get the workspace root.
 If NODEFAULT is t, return nil instead of `default-directory' if directory is
@@ -64,7 +63,7 @@ not in a rust project."
                                  (json-read-from-string
                                   (shell-command-to-string "cargo metadata --format-version 1")))
                     (error nil))
-                  default-directory)))
+                  (locate-dominating-file default-directory "Cargo.toml"))))
     (if dir
         (expand-file-name dir)
       (if nodefault
