47302dcafcb69f2ccad6116508089cfd5cd30074
Sam Hedin <sam.hedin@gmail.com>
2020-11-06 23:35:41 +0100

    Revert "Use cargo metadata to find workspace root"
    This reverts commit 4b1fa6c81c1a0ab139baf588159e957cd7b5a77a.

--- rustic-common.el
+++ rustic-common.el
@@ -59,12 +59,8 @@ non-nil."
   "Get the workspace root.
 If NODEFAULT is t, return nil instead of `default-directory' if directory is
 not in a rust project."
-  (let ((dir (or  (condition-case nil
-                      (concat  (alist-get '\workspace_root
-                                          (json-read-from-string
-                                           (shell-command-to-string "cargo metadata --format-version 1"))) "/")
-                    (error nil))
-                  default-directory)))
+  (let ((dir (locate-dominating-file
+              (or buffer-file-name default-directory) "Cargo.toml")))
     (if dir
         (expand-file-name dir)
       (if nodefault
