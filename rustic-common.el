30e75ae36bc3f6cdd7849ad4ce46d2e71b47e91d
Jonas Bernoulli <jonas@bernoul.li>
2021-04-04 23:35:00 +0200

    Create rustic-rustfmt.el from existing code

--- rustic-common.el
+++ rustic-common.el
@@ -16,47 +16,11 @@
   :link '(url-link "https://www.rustic-lang.org/")
   :group 'languages)
 
-(defcustom rustic-rustfmt-bin "rustfmt"
-  "Path to rustfmt executable."
-  :type 'string
-  :group 'rustic)
-
-(defcustom rustic-rustfmt-config-alist nil
-  "An alist of (KEY . VAL) pairs that are passed to rustfmt.
-
-KEY is a symbol that corresponds to a config value of rustfmt.
-VALUE is a string, an integer or a boolean."
-  :type '(alist :key-type symbol
-                :value-type (choice string integer boolean))
-  :group 'rustic)
-
 (defcustom rustic-lsp-setup-p t
   "Setup LSP related stuff automatically."
   :type 'boolean
   :safe #'booleanp
   :group 'rustic)
-
-(defcustom rustic-format-trigger nil
-  "Format future rust buffers before saving using rustfmt."
-  :type '(choice (const :tag "Format buffer before saving." on-save)
-                 (const :tag "Run 'cargo fmt' before compilation." on-compile)
-                 (const :tag "Don't format automatically." nil))
-  :group 'rustic)
-
-;;; Obsolete
-
-(defvar rustic-format-on-save nil
-  "Format rust buffers before saving using rustfmt.")
-(make-obsolete 'rustic-format-on-save 'rustic-format-trigger "Rustic 0.19")
-
-(defun rustic-format-on-save-p ()
-  "Return non-nil if formatting should happen when saving.
-See option `rustic-format-trigger'.  For backward compatibility,
-if obsolete `rustic-format-on-save' is non-nil, then also return
-non-nil."
-  (or rustic-format-on-save (eq rustic-format-trigger 'on-save)))
-
-;;; Essentials
 
 (defun rustic-buffer-workspace (&optional nodefault)
   "Get the workspace root.
@@ -68,14 +32,6 @@ not in a rust project."
         (expand-file-name dir)
       (if nodefault
           nil default-directory))))
-
-(defun rustic-compute-rustfmt-args ()
-  "Compute the arguments to rustfmt from `rustic-rustfmt-config-alist'."
-  (let (args)
-    (cl-dolist (elem rustic-rustfmt-config-alist args)
-      (cl-destructuring-bind (key . val) elem
-        (push (format "%s=%s" key (if (booleanp val) (if val "true" "false") val)) args)
-        (push "--config" args)))))
 
 ;;; Spinner
 
