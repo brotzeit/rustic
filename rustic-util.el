f6d51334052cf29fc30a06adaa3f51d7c2bada39
Jonas Bernoulli <jonas@bernoul.li>
2021-04-04 23:39:00 +0200

    rustic-open-dependency-file: Move to rustic-interaction.el

--- rustic-util.el
+++ rustic-util.el
@@ -26,18 +26,6 @@
                  function)
   :group 'rustic)
 
-;;; Interactive
-
-;;;###autoload
-(defun rustic-open-dependency-file ()
-  "Open the 'Cargo.toml' file at the project root if the current buffer is
-visiting a project."
-  (interactive)
-  (let ((workspace (rustic-buffer-workspace t)))
-    (if workspace
-        (find-file (concat workspace "/Cargo.toml"))
-      (message "The current buffer is not inside a rust project!"))))
-
 ;;; _
 (provide 'rustic-util)
 ;;; rustic-util.el ends here
