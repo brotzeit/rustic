635cc9aa9f7ee8a83095a4ae7e5c6c7393ca5735
Sam Hedin <sam.hedin@gmail.com>
2022-01-25 19:03:59 +0100

    Save location

--- test/rustic-doc-test.el
+++ test/rustic-doc-test.el
@@ -9,5 +9,6 @@
   (should (file-exists-p rustic-doc-convert-prog))
   (should (file-exists-p rustic-doc-lua-filter))
   (should (file-exists-p "~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/share/doc/rust/html/std/option"))
+  (print (format "rustic document location: %s" (concat rustic-doc-save-loc "/std/option/")))
   (print (format "files in std option: %s" (directory-files (concat rustic-doc-save-loc "/std/option/"))))
   (should (file-exists-p (concat rustic-doc-save-loc "/std/option/enum.Option.org"))))
