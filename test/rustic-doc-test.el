c90a9aabbd4349845462f144858d9204ba7cbcb6
Sam Hedin <sam.hedin@gmail.com>
2022-01-27 21:45:10 +0100

    Fix link to convert.sh

--- test/rustic-doc-test.el
+++ test/rustic-doc-test.el
@@ -6,10 +6,10 @@
 
 (ert-deftest rustic-doc-setup-test ()
   (rustic-doc-setup nil t)
-  (while (get-buffer (concat "*" "rustic-doc-std-conversion" "*"))
     ;;  The setup is async, so we can't continue immediately.
-    (sleep-for 1))
   (should (file-exists-p rustic-doc-convert-prog))
   (should (file-exists-p rustic-doc-lua-filter))
+  (while (get-buffer (concat "*" "rustic-doc-std-conversion" "*"))
+    (sleep-for 1))
   (should (file-exists-p "~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/share/doc/rust/html/std/option"))
   (should (file-exists-p (f-join rustic-doc-save-loc "std" "option" "enum.Option.org"))))
