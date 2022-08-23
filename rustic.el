ba1de51d781c8c559d623d897f8090c93a7a9039
Houjun Liu <houjun@jemoka.com>
2022-08-23 15:48:50 -0700

    Update rustic.el

--- rustic.el
+++ rustic.el
@@ -79,7 +79,7 @@
         (setq-local process-environment env)
         ;; Set PATH so we can find cargo.
         (setq-local exec-path path)
-        (let ((ret (call-process (rustic-cargo-bin) nil (list (current-buffer) nil) nil "locate-project" "--workspace")))
+        (let ((ret (process-file (rustic-cargo-bin) nil (list (current-buffer) nil) nil "locate-project" "--workspace")))
           (cond ((and (/= ret 0) nodefault)
                  (error "`cargo locate-project' returned %s status: %s" ret (buffer-string)))
                 ((and (/= ret 0) (not nodefault))
