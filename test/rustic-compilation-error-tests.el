ecd95edfdf4845fddd5e201db3d8caebe5a715ca
Sibi Prabakaran <sibi@psibi.in>
2024-02-19 10:30:52 +0530

    Fix one more flaky test

--- test/rustic-compilation-error-tests.el
+++ test/rustic-compilation-error-tests.el
@@ -133,8 +133,7 @@
       (rustic-cargo-build)
       (let* ((proc (get-process rustic-compilation-process-name))
              (buffer (process-buffer proc)))
-        (while (eq (process-status proc) 'run)
-          (sit-for 0.01))
+        (rustic-test--wait-till-finished buffer)
         (with-current-buffer buffer
           (goto-char (point-min))
           (when (re-search-forward "-->")
